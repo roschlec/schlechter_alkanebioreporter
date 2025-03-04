fac866de2888016ff36bbea52c5957e023008c48
roschlec <roschlec@gmail.com>
2025-03-04 14:53:53 +0100

    Scripts for in vitro analysis

---
+++ src/01_invitro_cfu.R
@@ -0,0 +1,63 @@
+#### In vitro data ####
+
+##  Libraries
+library(tidyverse)
+library(here)
+library(scales)
+library(RColorBrewer)
+library(ggpubr)
+library(patchwork)
+library(moments)
+library(rstatix)
+library(ggprism)
+
+source(here('src', '00_dataclean.R'))
+
+##  Growth in BHB   ####
+#   OD
+od <- 
+    read.csv(here('data', 'invitro_od.csv')) %>% 
+    na.exclude() %>% 
+    group_by(replicate, time_d, treatment) %>% 
+    summarise(od = mean(od),
+              .groups = "drop") %>% 
+    arrange(treatment, time_d)
+
+#  Summary
+od %>% 
+    group_by(time_d, treatment) %>% 
+    summarise(
+        mean = mean(od),
+        sd = sd(od)) %>% 
+    filter(treatment == "diesel") %>% 
+    print(n = nrow(.))
+
+#   Plotting
+plt_od <-
+    od %>% 
+    group_by(time_d, treatment) %>% 
+    summarise(meanOD = mean(od),
+              sdOD = sd(od),
+              .groups = "drop") %>% 
+    ggplot(aes(x = time_d, y = meanOD, group = treatment))+
+    geom_point(data = od, aes(y = od, color = treatment), alpha = 0.8, size = 1)+
+    geom_line(linetype = "dashed", lineend = "round", alpha = 0.4)+
+    geom_errorbar(aes(ymin = meanOD-sdOD, ymax = meanOD+sdOD), width = 0.3)+
+    geom_point(aes(color = treatment), fill = "white", pch = 21, size = 2, alpha = 0.9, stroke = 1)+
+    scale_color_manual(name = "Treatment", labels = trt_invitro, values = palette_invitro)+
+    scale_y_continuous(name = bquote("Optical density ("*OD[600]*") [a.u.]"), limits = c(0,0.8))+
+    scale_x_continuous(name = "Time [d]", limits = c(-0.1, 30), breaks = seq(0,28,7))+
+    guides(color = guide_legend(override.aes = list(alpha = 1, size = 4, fill = palette_invitro, stroke = 0)))+
+    theme(
+        axis.text = element_text(size = 6, color = "black"),
+        axis.title = element_text(size = 8, color = "black"),
+        legend.text = element_text(size = 6, color = "black"),
+        legend.title = element_text(size = 8, color = "black"),
+        strip.background = element_rect(fill = NA),
+        panel.border = element_rect(linewidth = 0.5, fill = NA),
+        panel.background = element_rect(fill = "white"),
+        axis.ticks.length = unit(1.5, "mm"),
+        axis.ticks = element_line(linewidth = 0.25))
+
+plt_od
+ggsave(here('results', 'invitro_od.png'), dpi = 300, width = 6, height = 4)
